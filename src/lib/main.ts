1e753c21ae3705bbdb7730d17188084a22c610e8
Thomas <52868615+Sigeth@users.noreply.github.com>
2021-05-29 02:27:27 +0200

    Created the module

---
+++ src/lib/main.ts
@@ -0,0 +1,10 @@
+/** @internal */
+export const urls = {
+    codingamer: "https://www.codingame.com/services/CodinGamer/"
+}
+
+
+/** @internal */
+export const defaultHeaders = {
+    "content-type": "application/json;charset=UTF-8"
+}
