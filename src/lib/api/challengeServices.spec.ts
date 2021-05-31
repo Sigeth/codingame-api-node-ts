ecb42d72380d3cea5d4cb5710bff884075864499
Thomas <52868615+Sigeth@users.noreply.github.com>
2021-05-31 23:47:24 +0200

    Added getChallengeByPublicId

--- src/lib/api/challengeServices.spec.ts
+++ src/lib/api/challengeServices.spec.ts
@@ -3,6 +3,7 @@ import anyTest, { TestInterface } from 'ava'
 const fs = require("fs")
 
 import {
+    getChallengeByPublicId,
     getPastChallenges
     } from "./challengeServices"
 
@@ -29,6 +30,18 @@ test("Get the past challenges", async t => {
         const title = pastChallenges[0].title
         
         t.assert(pastChallenges[0].title === title)
+
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+test("Get a challenge by its public id", async t => {
+    try {
+
+        const challenge = await getChallengeByPublicId("spring-challenge-2021")
+        
+        t.assert(challenge.challenge.title === "Spring Challenge 2021")
 
     } catch (e) {
         console.log(e)
--- src/lib/api/challengeServices.ts
+++ src/lib/api/challengeServices.ts
@@ -28,6 +28,27 @@ export const getPastChallenges = async (cookies: string, userId: number): Promis
     return response["data"]
 }
 
+/**
+ * Get a challenge with its public ID
+ * 
+ * @param {string} publicId - Challenge's public ID
+ * 
+ */
+
+export const getChallengeByPublicId = async (publicId: string): Promise<IChallenge> => {
+    
+    const response = await axios({
+        url: urls.challenge + "findWorldCupByPublicId",
+        method: "post",
+        headers: {
+            "content-type": "application/json;charset=UTF-8"
+        },
+        data: [ publicId, null ]
+    })
+
+    return response["data"]
+}
+
 
 export interface IPastChallenge {
     title: string
@@ -38,4 +59,71 @@ export interface IPastChallenge {
     descriptionJson: string | undefined
     endApplicationsDate: number
     applicationsClosed: boolean
+}
+
+export interface IChallenge {
+    challenge: challenge
+    jobOffers: any[]
+    teasers: any[]
+}
+
+type challenge = {
+    challengeId: number
+    title: string
+    date: number
+    utc: number
+    enable: boolean
+    testId: number
+    placesOnlineAvailable: number
+    placesOnSiteAvailable: number
+    placesOnlineMax: number
+    placesOnSiteMax: number
+    challengerSubscriptionSeq: number
+    registeredChallengerCount: number
+    subscriptionOpened: boolean
+    challengeForm: {}
+    visible: boolean
+    enableSelectCompany: boolean
+    reportEnabled: boolean
+    lateTimeMax: number
+    subscribedLabelId: number
+    needLogin: boolean
+    enableScoreGlobal: boolean
+    jobScoreGlobal: boolean
+    mailinglist: boolean
+    publicId: string
+    enableIcal: boolean
+    mailGroupId: number
+    type: string
+    startInMillis: number
+    rankingCompleted: boolean
+    needPreRegistration: boolean
+    shareable: boolean
+    delayRedirectRankingWhenFinished: number
+    applicationEndDate: number
+    applicationContactEndDate: number
+    training: boolean
+    vip: boolean
+    finished: boolean
+    closed: boolean
+    started: boolean
+    privateEvent: boolean
+    defaultAi: boolean
+    logoId: number
+    cover1Id: number
+    cover2Id: number
+    coverMetaId: number
+    descriptionJson: string
+    registrationEnable: boolean
+    isolated: boolean
+    enableLeaderboard: boolean
+    enableSocialLogin: boolean
+    endApplicationsDate: number
+    applicationsClosed: boolean
+    customFormId: number
+    authors: any[]
+    clashHubs: any[]
+    formFieldsShownInLeaderboard: any[]
+    enableSchoolCompanyLeaderboard: boolean
+    disableReplays: boolean
 }
