43959d281298a455956bf150edb7775094c63a2e
Thomas <52868615+Sigeth@users.noreply.github.com>
2021-06-02 00:57:25 +0200

    Added getPublishedCoursesByIds

--- src/lib/api/courseServices.spec.ts
+++ src/lib/api/courseServices.spec.ts
@@ -1,7 +1,34 @@
 import anyTest, { TestInterface } from 'ava'
 
-const test = anyTest as TestInterface<{}>
+const fs = require("fs")
 
-test("void", async t => {
-    t.assert(true)
+import {
+    getPublishedCoursesByIds
+    } from "./courseServices"
+
+const test = anyTest as TestInterface<{ cookies: string }>
+
+test.before(async t => {
+    while(t.context.cookies === undefined){
+        try {
+            const storedCookies = fs.readFileSync("./src/config/cookies.json", "utf-8")
+
+            const parsedCookies = JSON.parse(storedCookies)
+            const cookies = parsedCookies.cookies
+
+            t.context.cookies = cookies
+        } catch (e) {}
+    }
+})
+
+test("Get some courses infos", async t => {
+    try {
+
+        const courses = await getPublishedCoursesByIds(t.context.cookies, 4365603, [57133, 56997])
+        
+        t.assert(courses !== undefined)
+
+    } catch (e) {
+        console.log(e)
+    }
 })
---
+++ src/lib/api/courseServices.ts
@@ -0,0 +1,59 @@
+import axios from "axios"
+
+import { urls } from "../main"
+
+
+/**
+ * Get CodinGamer's stats and points
+ * 
+ * ## Requires to log in before.
+ * 
+ * @param {string} cookies - The cookies string that you obtain when you're logging in with loginCodinGamer
+ * @param {number} userId - User's ID
+ * @param {number[]} coursesId - List containing every courses you want to get infos
+ * 
+ */
+
+export const getPublishedCoursesByIds = async (cookies: string, userId: number, coursesId: number[]): Promise<ICourse> => {
+    
+    const response = await axios({
+        url: urls.course + "findPublishedCoursesByIdsAndUserId",
+        method: "post",
+        headers: {
+            "content-type": "application/json;charset=UTF-8",
+            "cookie": cookies
+        },
+        data: [ userId, coursesId ]
+    })
+
+    return response["data"]
+}
+
+export interface ICourse {
+    id: number
+    title: string
+    lastVersionId: number
+    courseCreatorId: number
+    publicPath: string
+    author: author
+    coverBinaryId: number
+    views: number
+    likes: number
+    liked: boolean
+    disqusIdentifier: string
+    firstPublishedDate: number
+    contentLanguage: string
+    tagIds: number[]
+}
+
+type author = {
+    userId: number
+    pseudo: string | undefined
+    publicHandle: string
+    enable: boolean
+    avatar: number | undefined
+    cover: number | undefined
+    tagline: string | undefined
+    level: number
+    biography: string | undefined
+}
