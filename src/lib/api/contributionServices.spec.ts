d0fc4967b472175bfc7d40205c3d1f4ae58400e7
Thomas <52868615+Sigeth@users.noreply.github.com>
2021-06-01 20:30:32 +0200

    Added getContributionById

--- src/lib/api/contributionServices.spec.ts
+++ src/lib/api/contributionServices.spec.ts
@@ -3,6 +3,7 @@ import anyTest, { TestInterface } from 'ava'
 const fs = require("fs")
 
 import {
+    getContributionById,
     getPendingContributions
     } from "./contributionServices"
 
@@ -27,6 +28,18 @@ test("Get pending contributions", async t => {
         const pendingContributions = await getPendingContributions(t.context.cookies, 1,"ALL",4365603)
         
         t.assert(pendingContributions !== undefined)
+
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+test("Get a contribution by its id", async t => {
+    try {
+
+        const contribution = await getContributionById("6648d5564874df8f45e4f0a79f17e723fb02")
+        
+        t.assert(contribution !== undefined)
 
     } catch (e) {
         console.log(e)
--- src/lib/api/contributionServices.ts
+++ src/lib/api/contributionServices.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { SecureVersion } from "tls"
 
 import { urls } from "../main"
 
@@ -25,6 +26,28 @@ export const getPendingContributions = async (cookies: string, page: number, fil
             "cookie": cookies
         },
         data: [ page, filter, userId ]
+    })
+
+    return response["data"]
+}
+
+
+/**
+ * Get every pending contributions, that wait to be validated
+ * 
+ * @param {string} contributionId - Contribution's ID, found in the URL.
+ * 
+ */
+
+ export const getContributionById = async (contributionId: string): Promise<IContribution> => {
+    
+    const response = await axios({
+        url: urls.contribution + "findContribution",
+        method: "post",
+        headers: {
+            "content-type": "application/json;charset=UTF-8"
+        },
+        data: [ contributionId, true ]
     })
 
     return response["data"]
@@ -73,4 +96,73 @@ type statusHistory = {
 type dataStatus = {
     author: string
     reason: string
+}
+
+export interface IContribution {
+    id: number
+    activeVersion: number
+    score: number
+    votableId: number
+    codingamerId: number
+    views: number
+    commentableId: number
+    title: string
+    status: string
+    type: string
+    nickname: string
+    publicHandle: string
+    codingamerHandle: string
+    lastVersion: version
+    avatar: number
+    commentCount: number
+    upVotes: number
+    downVotes: number
+    validateAction: actionStatus | undefined
+    denyAction: actionStatus | undefined
+    statusHistory: statusHistory[]
+    editable: boolean
+    draft: boolean
+    readyForModeration: boolean
+}
+
+type version = {
+    version: number
+    autocloseTime: number
+    data: versionData
+    draft: boolean
+    readyForModeration: boolean
+    statementHTML: string
+}
+
+type versionData = {
+    title: string
+    topics: topicVersion[]
+    statement: string
+    testCases: testCase[]
+    difficulty: string
+    constraints: string
+    coverBinaryId: number
+    stubGeneration: string
+    inputDescription: string
+    outputDescription: string
+}
+
+type topicVersion = {
+    id: number | undefined
+    handle: string | undefined
+    category: string | undefined
+    labelMap: { "1": string | undefined, "2": string }
+    pageTitle: string | undefined
+    puzzleCount: number | undefined
+    parentTopicId: number | undefined
+    contentDetailsId: number | undefined
+}
+
+type testCase = {
+    title: string
+    isTest: boolean
+    testIn: string
+    testOut: string
+    isValidator: boolean
+    needValidation: boolean
 }
